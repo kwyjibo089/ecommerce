faefcdade3e5e3a2403bec3bdd5fb55a0090c2ad
Roman Müller <romanmueller@gmx.ch>
2022-04-07 21:39:37 +0200

    fetch countries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
-import { Products, Navbar, Cart } from "./components";
+import { Products, Navbar, Cart, Checkout } from "./components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -65,7 +65,8 @@ const App = () => {
                 handleEmptyCart={handleEmptyCart}
               />
             }
-          ></Route>
+          />
+          <Route exact path="/checkout" element={<Checkout cart={cart} />} />
         </Routes>
       </div>
     </Router>
