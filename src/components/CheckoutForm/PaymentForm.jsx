f032146a7d2399c44cf849fdddebca7a09a93262
Roman Müller <romanmueller@gmx.ch>
2022-04-22 16:17:01 +0200

    review

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Typography, Button, Divider } from "@material-ui/core";
+import {
+  Element,
+  CardElement,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { Review } from "./Review";
 
-const PaymentForm = () => {
-  return <div>PaymentForm</div>;
+const PaymentForm = ({ checkoutToken }) => {
+  return <div>{<Review checkoutToken={checkoutToken} />}</div>;
 };
 
 export default PaymentForm;
